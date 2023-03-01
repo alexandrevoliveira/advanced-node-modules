47d61aa280993561b604d38a8eca7889556d58ad
Alexandre Oliveira <alexandre.oliveira@liferay.com>
2023-03-01 11:42:17 -0300

    refactor: add multiple sockets logic

--- networking/net.js
+++ networking/net.js
@@ -1,16 +1,23 @@
-process.stdout.write('\u001B[2J\u001B[0;0f')
-
 const server = require('net').createServer()
+let counter = 0
+let sockets = {}
 
 server.on('connection', socket => {
+  socket.id = counter++
+  sockets[socket.id] = socket
+
   console.log('Client connected')
   socket.write('Welcome new client!\n')
+
   socket.on('data', data => {
-    console.log('data is:', data)
-    socket.write('data is: ')
-    socket.write(data)
+    Object.entries(sockets).forEach(([_, clientSocket]) => {
+      clientSocket.write(`${socket.id}: `)
+      clientSocket.write(data)
+    })
   })
+
   socket.on('end', () => {
+    delete sockets[socket.id]
     console.log('Client disconnected')
   })
   // socket.setEncoding('utf8') //sets all encoding to utf8 (data comes as buffer by default)
