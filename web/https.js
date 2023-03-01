6aa5e6120ac51d95fcaa450be30d0d3d993eb396
Alexandre Oliveira <alexandre.oliveira@liferay.com>
2023-03-01 13:28:23 -0300

    feat: add basic https example

---
+++ web/https.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const server = require('https')
+  .createServer({
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem')
+  })
+
+server.on('request', (req, res) => {
+  res.writeHead(200, { 'content-type': 'text/plain' })
+  res.end('Hello world!\n')
+})
+
+server.listen(443)
+
+// generate a cert command:
+// openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -nodes
