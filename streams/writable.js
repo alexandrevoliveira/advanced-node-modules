f37642fa2684d585cad041c63031d3934b50cb80
Alexandre Oliveira <alexandre.oliveira@liferay.com>
2023-03-02 10:24:43 -0300

    feat: add 1st writable stream example

---
+++ streams/writable.js
@@ -0,0 +1,11 @@
+const { Writable } = require('stream')
+
+const outStream = new Writable({
+  write(chunk, encoding, callback) {
+    console.log(chunk.toString())
+    callback()
+  }
+})
+
+process.stdin.pipe(outStream)
+// same as process.stdin.pipe(process.stdout)
